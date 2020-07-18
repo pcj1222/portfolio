5641e70effc4b1ec0cb635d922222cf1dac2a57e
pcj1222 <alsgur124@gmail.com>
2020-07-18 17:36:11 +0900

    2

--- Index.jsx
+++ Index.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/Assets/theme';
 
 const Hot = hot(App);
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <ThemeProvider theme={theme}>
             <Hot />
         </ThemeProvider>
-    </HashRouter>, 
+    </BrowserRouter>, 
 document.querySelector('#root'));
